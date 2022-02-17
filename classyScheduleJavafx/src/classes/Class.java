03cb5ed65fd40ee9fbab236bb3af30dcfb608e9f
xion6136 <71159252+xion6136@users.noreply.github.com>
2022-02-17 17:17:27 -0600

    Add files via upload

---
+++ classyScheduleJavafx/src/classes/Class.java
@@ -0,0 +1,38 @@
+package classes;
+import java.util.*;
+
+public interface Class {
+    String getClassName();
+    void setClassName(String name);
+
+    int getIDNumber();
+    void setIDNumber(int number);
+
+    int getSectionNumber();
+    void setSectionNumber(int number);
+
+    String getDepartment();
+    void setDepartment(String department);
+
+    int getCapacity();
+    void setCapacity(int capacity);
+
+    //registered??
+
+    // This refers to whether the class is online/hybrid/in-person
+    String getModality();
+    void setModality(String modality);
+
+    int creditNumber();
+    void setCreditNumber(int creditNumber);
+
+    float teachLoad();
+    void setTeachLoad(float amount);
+
+    // I'm not sure about the "classes" type yet, so I just put in "Object" in for now.
+    List<Object> getPrerequisite();
+    void setPrerequisite(List<Object> classes);
+
+    List<Object> getConflicts();
+    void setConflicts(List<Object> classes);
+}
