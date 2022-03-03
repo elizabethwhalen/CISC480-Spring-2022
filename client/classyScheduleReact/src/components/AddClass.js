a202c29863572302fe93c8c9d3ad4f57254df3a0
Kim Tien <47434246+ktngn@users.noreply.github.com>
2022-03-02 23:09:56 -0600

    Add Calendar and AddClass components

---
+++ client/classyScheduleReact/src/components/AddClass.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// This is just a template, adjust according to how you like
+const AddClass = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddClass
---
+++ client/classyScheduleReact/src/components/Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// This is just a template, adjust according to how you like
+const Calendar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Calendar
