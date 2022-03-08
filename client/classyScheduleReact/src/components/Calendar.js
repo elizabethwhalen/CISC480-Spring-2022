9c743d61e7a04219b94777de63f7378b67ba8e06
Giblit42 <71239011+Giblit42@users.noreply.github.com>
2022-03-07 18:29:58 -0600

    update calendar

--- client/classyScheduleReact/src/components/Calendar.js
+++ client/classyScheduleReact/src/components/Calendar.js
@@ -1,11 +1,45 @@
 import React from 'react'
-
+import { Eventcalendar, getJson, toast } from '@mobiscroll/react';
 // This is just a template, adjust according to how you like
 const Calendar = () => {
-    return (
-        <div>
 
-        </div>
+
+
+        const [myEvents, setEvents] = React.useState([]);
+    
+        React.useEffect(() => {
+            getJson('https://trial.mobiscroll.com/events/?vers=5', (events) => {
+                setEvents(events);
+            }, 'jsonp');
+        }, []);
+        
+        const onEventClick = React.useCallback((event) => {
+            toast({
+                message: event.event.title
+            });
+        }, []);
+        
+        const view = React.useMemo(() => {
+            return {
+                schedule: { type: 'week' }
+            };
+        }, []);
+
+
+    return (
+        
+            <Eventcalendar
+            theme="ios" 
+            themeVariant="light"
+            clickToCreate={true}
+            dragToCreate={true}
+            dragToMove={true}
+            dragToResize={true}
+            data={myEvents}
+            view={view}
+            onEventClick={onEventClick}
+            />
+        
     )
 }
 
