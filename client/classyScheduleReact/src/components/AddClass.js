2cca290313b688f302a00578219207d51c888519
Antonio Palmeros <palm1403@stthomas.edu>
2022-03-08 02:13:41 -0600

    add class functionality, connection requires secure transport

--- client/classyScheduleReact/src/components/AddClass.js
+++ client/classyScheduleReact/src/components/AddClass.js
@@ -6,6 +6,7 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { makeStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
+import Axios from 'axios';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -28,10 +29,28 @@ const useStyles = makeStyles((theme) => ({
 const AddClass = () => {
     const [code, setCode] = React.useState('');
     const [courseNum, setCourseNum] = React.useState('');
+    const [courseName, setCourseName] = React.useState('');
 
-    const handleChange = (event) => {
+    const submitForm = () => {
+        Axios.post('http://localhost:3000/AddClass', {
+            dept_code: code,
+            class_num: courseNum,
+            class_name: courseName,
+        }).then(() => {
+            alert('inserted');
+        });
+    };
+
+    const handleChangeCode = (event) => {
+        //console.log('typed => ${event.target.value}');
         setCode(event.target.value);
     };
+    const handleChangeCourseNum = (event) => {
+        setCourseNum(event.target.value);
+    }
+    const handleChangeCourseName = (event) => {
+        setCourseName(event.target.value);
+    }
     const classes = useStyles()
 
     return (
@@ -51,7 +70,7 @@ const AddClass = () => {
                             id="demo-simple-select"
                             value={code}
                             label="code"
-                            onChange={handleChange}
+                            onChange={handleChangeCode}
                             size='medium'
                             autoWidth
                         >
@@ -64,13 +83,13 @@ const AddClass = () => {
                     </FormControl>
                 </Grid>
                 <Grid item xs={4} fullWidth>
-                    <TextField fullWidth size="medium" id="outlined-basic" label="Course #" variant="outlined" />
+                    <TextField fullWidth size="medium" id="outlined-basic" label="Course #" variant="outlined" value={courseNum} onChange={handleChangeCourseNum}  />
                 </Grid>
                 <Grid item xs={4} fullWidth>
-                    <TextField fullWidth size="medium" id="outlined-basic" label="Coure Name" variant="outlined" />
+                    <TextField fullWidth size="medium" id="outlined-basic" label="Course Name" variant="outlined" value={courseName} onChange={handleChangeCourseName}  />
                 </Grid>
                 <Grid item xs={4} fullWidth>
-                    <Button variant="contained" size="large" type="submit"  disableElevation>
+                    <Button variant="contained" size="large" type="submit"  disableElevation onClick={submitForm}>
                         Submit
                     </Button>
                     {/* <Link to='/Calendar'>
--- server/index.js
+++ server/index.js
@@ -1,23 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
+
 const db = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password: 'root',
-    database: 'testdb',
+    host:'classy-schedule-database.mysql.database.azure.com',
+    user:'db_test',
+    password: 'fA!6#_&eaU9-EaeJ',
+    database: 'class_schedule',
+});
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.post('/AddClass', (req, res) => {
+
+    const dept_code = req.body.dept_code;
+    const class_num = req.body.class_num;
+    const class_name = req.body.class_name;
+
+    const sqlInsert = "INSERT INTO class (dept_code, class_num, class_name) VALUES (?,?,?)"
+    db.query(sqlInsert, [dept_code, class_num, class_name], (err, result) => {
+        console.log(err)
+    });
 });
 
-app.get('/', (req, res) => {
-    sqlquery = "insert"
-    res.send('');
-})
-
-app.post('api/insert', (req, res)=>{
-    const  sqlInsert = 'INSERT INTO table (dept, dept_num, dept_name)';
-})
-
-app.listen(3001, () => {
-    console.log("running on port 3306");
-})
+app.listen(3000, () => {
+    console.log("running on port 3000");
+});
