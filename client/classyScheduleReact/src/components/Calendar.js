a35025547da068492f617697cda739a801b6d0b5
Giblit42 <71239011+Giblit42@users.noreply.github.com>
2022-03-10 11:43:14 -0600

    basic calendar

--- client/classyScheduleReact/src/components/Calendar.js
+++ client/classyScheduleReact/src/components/Calendar.js
@@ -1,41 +1,101 @@
+import { Grid, Paper } from '@material-ui/core'
 import React from 'react'
-import { Eventcalendar, getJson, toast } from '@mobiscroll/react';
+
+
 // This is just a template, adjust according to how you like
+
+
+
+
 const Calendar = () => {
-        const [myEvents, setEvents] = React.useState([]);
     
-        React.useEffect(() => {
-            getJson('https://trial.mobiscroll.com/events/?vers=5', (events) => {
-                setEvents(events);
-            }, 'jsonp');
-        }, []);
-        
-        const onEventClick = React.useCallback((event) => {
-            toast({
-                message: event.event.title
-            });
-        }, []);
-        
-        const view = React.useMemo(() => {
-            return {
-                schedule: { type: 'week' }
-            };
-        }, []);
-
-
     return (
+        <Paper>
+            <Grid Grid item xs={12} fullWidth>
+                <div class='header'>
+                    <h1>Classy Scheduler</h1>
+                
+                </div>
+            </Grid>
+            
+            <Grid item xs={12} fullWidth>
+                
+                
+                <table >
+                    <tr>
+                        <th>Time</th>
+                        <th>Monday</th>
+                        <th> Tuesday</th>
+                        <th> Wednessday</th>
+                        <th> Thursday</th>
+                        <th> Friday</th>
+                    </tr>
+                    <tr>
+                        <td>8:15-9:20</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                    </tr>
+                    <tr>
+                        <td>9:35-10:40</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                    </tr>
+                    <tr>
+                        <td>10:55-12:00</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                    </tr>
+                    <tr>
+                        <td>12:15-1:20</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                    </tr>
+                    <tr>
+                        <td>1:35-2:40</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                    </tr>
+                    <tr>
+                        <td>2:55-4:20</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                    </tr>
+                    <tr>
+                        <td>4:35-5:40</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                        <td> class</td>
+                    </tr>
+                                
+                </table>
+                
+                
+            </Grid>
+            
+            
+        </Paper>
         
-            <Eventcalendar
-            theme="ios" 
-            themeVariant="light"
-            clickToCreate={true}
-            dragToCreate={true}
-            dragToMove={true}
-            dragToResize={true}
-            data={myEvents}
-            view={view}
-            onEventClick={onEventClick}
-            />
+            
         
     )
 }
