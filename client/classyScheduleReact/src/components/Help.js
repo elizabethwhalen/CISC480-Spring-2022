46a2e678a317d2854e202280c32c9f9761cd4486
Giblit42 <71239011+Giblit42@users.noreply.github.com>
2022-03-28 17:03:50 -0500

    added help page

---
+++ client/classyScheduleReact/src/components/Help.js
@@ -0,0 +1,18 @@
+import { Grid, Paper } from '@material-ui/core'
+import React from 'react'
+
+const Help = () => {
+
+     return(
+         <Paper>
+             <Grid container>
+                 <Grid item xs = {12}>
+                    <div class= "header">
+                        <h1>Frequently Asked Questions</h1>
+                    </div>
+                 </Grid>
+             </Grid>
+         </Paper>
+     )
+}
+export default Help
--- client/classyScheduleReact/src/router/AppRouter.js
+++ client/classyScheduleReact/src/router/AppRouter.js
@@ -3,6 +3,7 @@ import AddClass from '../components/AddClass'
 import Calendar from '../components/Calendar'
 import Home from '../components/Home'
 import AddFaculty from '../components/AddFaculty'
+import Help from '../components/Help'
 
 const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
       <Route exact path="/AddClass" element={<AddClass/>} />
       <Route exact path="/Calendar" element={<Calendar/>} />
       <Route exact path="/AddFaculty" element={<AddFaculty/>} />
+      <Route exact path="/Help" element={<Help/>}/>
     </Routes>
   )
 }
