5524fea9a456bdd7ca7166eafed954e060b53d27
elizabethwhalen <78924208+elizabethwhalen@users.noreply.github.com>
2022-03-30 18:35:14 -0500

    updates so merge worked

--- ClassySchedule/src/main/java/homescreen/HomescreenController.java
+++ ClassySchedule/src/main/java/homescreen/HomescreenController.java
@@ -3,6 +3,7 @@ package homescreen;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -58,10 +59,8 @@ public class HomescreenController implements Initializable {
         homescreenStage.show();
     }
 
-    /**
-     * Changes scene to add classroom page when 'Add Classroom' button is clicked
-     */
-    public void addClassroomButtonClicked(MouseEvent mouseEvent) {
+
+    /*public void addClassroomButtonClicked(MouseEvent mouseEvent) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/ClassroomNew.fxml"));
         Parent root = null;
         try {
@@ -74,11 +73,35 @@ public class HomescreenController implements Initializable {
         homescreenStage.setTitle("Classy-Schedule");
         homescreenStage.setScene(new Scene(root, 600, 400));
         homescreenStage.show();
+    }*/
+
+    /**
+     * Changes scene to add classroom page when 'Add CLassroom' button is clicked
+     */
+    public void addClassroomButtonClicked(MouseEvent mouseEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/resources/fxml/ClassroomNew.fxml"));
+            homescreenStage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            homescreenStage.setScene(scene);
+            homescreenStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Changes scene to add professor page when 'Add Professor' button is clicked
      */
     public void addProfessorButtonClicked(MouseEvent mouseEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/resources/fxml/Professor.fxml"));
+            homescreenStage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            homescreenStage.setScene(scene);
+            homescreenStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
