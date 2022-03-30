229642e276660c7ba217d260dbc6214f5b708e3a
Antonio Palmeros <palm1403@stthomas.edu>
2022-03-30 14:34:41 -0500

    Added comments and completed 'Help' tab

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// all required npm modules for backend connectivity
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -11,23 +12,27 @@ const db = mysql.createPool({
     password: 'fA!6#_&eaU9-EaeJ',
     database: 'class_schedule',
 });
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors()); // initializing cors
+app.use(express.json()); // initializing express
+app.use(bodyParser.urlencoded({extended:true})); // initializing bodyParser
 
-
+// http post call to the /AddClass api, allows us to add classes to database.
 app.post('/AddClass', (req, res) => {
 
+    // dynamic variables that change when post is executed.
     const dept_code = req.body.dept_code;
     const class_num = req.body.class_num;
     const class_name = req.body.class_name;
 
+    // sql query 'INSERT' which adds the string representation of our variables into the database.
+    // this will be modified to a json once database team gets their api on azure running.
     const sqlInsert = "INSERT INTO class (dept_code, class_num, class_name) VALUES (?,?,?)"
     db.query(sqlInsert, [dept_code, class_num, class_name], (err, result) => {
         console.log(err)
     });
 });
 
+// activates when server starts. logs the port that the server is running on, planning to switch to azure port.
 app.listen(3000, () => {
     console.log("running on port 3000");
 });
