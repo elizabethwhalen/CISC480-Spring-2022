01b040f6308e7b8eb33c48f10691b47811c8390a
Schuster-Zack <schu9311@stthomas.edu>
2022-03-30 18:04:49 -0600

    can get to add professor now

--- ClassySchedule/src/main/java/homescreen/HomescreenController.java
+++ ClassySchedule/src/main/java/homescreen/HomescreenController.java
@@ -3,9 +3,9 @@ package homescreen;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -13,38 +13,54 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import courses.CourseController;
+
+        import courses.CourseController;
+        import homescreen.HomescreenController;
+        import javafx.fxml.FXML;
+        import javafx.fxml.FXMLLoader;
+        import javafx.fxml.Initializable;
+        import javafx.scene.Parent;
+        import javafx.scene.Scene;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.TextField;
+        import javafx.stage.Stage;
+
+        import java.io.IOException;
+        import java.net.URL;
+        import java.util.ResourceBundle;
 
 /**
- *Controls the homescreen page, which allows the user to navigate to add course, add professor, add classroom, and view
- * schedule pages.
+ *
  */
 public class HomescreenController implements Initializable {
     @FXML
-    public Button addCourse;
-    public Button addClassroom;
-    public Button addProfessor;
+    public Button addcourse;
+    public Button addclassroom;
+    public Button addprofessor;
 
     private Stage homescreenStage;
 
-    public HomescreenController() {
-    }
+    public HomescreenController() {}
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
     }
 
-
+    /**
+     * Sets stage to homescreen
+     * @param stage
+     */
     public void setStage(Stage stage) {
+
         this.homescreenStage = stage;
     }
 
-    /**
-     * Changes scene to add course page when 'Add Course' button is clicked
-     */
+
     @FXML
-    public void addCourseButtonClicked() {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/AddCourse.fxml"));
+    public void addcourseButtonClicked() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/addcourse.fxml"));
         Parent root = null;
         try {
             root = loader.load();
@@ -58,27 +74,19 @@ public class HomescreenController implements Initializable {
         homescreenStage.show();
     }
 
-    /**
-     * Changes scene to add classroom page when 'Add Classroom' button is clicked
-     */
-    public void addClassroomButtonClicked(MouseEvent mouseEvent) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/ClassroomNew.fxml"));
-        Parent root = null;
-        try {
-            root = loader.load();
-            CourseController homescreenController = loader.getController();
-            homescreenController.setStage(homescreenStage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        homescreenStage.setTitle("Classy-Schedule");
-        homescreenStage.setScene(new Scene(root, 600, 400));
-        homescreenStage.show();
+    public void addclassroomButtonClicked(MouseEvent mouseEvent) {
+
     }
 
-    /**
-     * Changes scene to add professor page when 'Add Professor' button is clicked
-     */
-    public void addProfessorButtonClicked(MouseEvent mouseEvent) {
+    public void addprofessorButtonClicked(MouseEvent mouseEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/resources/fxml/Professor.fxml"));
+            homescreenStage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            homescreenStage.setScene(scene);
+            homescreenStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
