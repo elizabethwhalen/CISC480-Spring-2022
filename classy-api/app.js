fd31615614afc3c1920619136598fc7da3c9fd4b
joelambrecht <lamb2290@stthomas.edu>
2022-05-04 20:07:26 -0500

    fx readded module.exports

--- classy-api/app.js
+++ classy-api/app.js
@@ -125,6 +125,8 @@ app.post('/v3/class', async (req, res) => {
     const status=verifyOutput[0]
     const payload=verifyOutput[1]
     if (status != 200){res.status(status).send(payload)}
+    //auth verified. Only access_level 2 (admin) can use this method.
+    else if (payload.user.access_level!=2){res.status(403).send("REQUEST DENIED- admin method only")}
     //auth validated
     else{
         //check if new dept
@@ -980,9 +982,7 @@ app.get('/v2/faculty_class', (req, res) => {
     const status=verifyOutput[0]
     const payload=verifyOutput[1]
     if (status != 200){res.status(status).send(payload)}
-    console.log(payload)
-    console.log(payload.user.access_level)
-    if (payload.user.access_level==0){res.status(401).send("Unauthorized")}
+    else if (payload.user.access_level<1){res.status(403).send("REQUEST DENIED- admin or faculty method only")}
     else{
         let query = "SELECT * FROM faculty_class";
 
@@ -3030,6 +3030,7 @@ function verify(token){
 	}
   return [200,payload]
 }
+
 
 // *** v0 is deprecated ***
 /*
@@ -6739,6 +6740,6 @@ app.put('/title', (req, res) => {
   })
   //res.redirect('/title')
 });
+*/
 
 module.exports = app;
-*/
